220efdb4ad8039721b1594829232beda1857b36d
KPMGE <jesuskevin254@gmail.com>
2020-08-31 19:09:19 -0300

    Adding program that calculates the distance among two points, using a structure to define a point

---
+++ structures/distance_structure.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+struct point 
+{
+    float x;
+    float y;
+};
+typedef struct point Point;
+
+float calculate_distance(Point* p1, Point* p2);
+
+int main(void)
+{
+    float distancePoints;
+    Point p1, p2;
+
+    cout << "Enter a point (x, y): ";
+    cin >> p1.x >> p1.y;
+    cout << "Enter a point (x, y): ";
+    cin >> p2.x >> p2.y;
+
+    distancePoints = calculate_distance(&p1, &p2);
+    
+    cout << "Distance = " << distancePoints;
+
+    return 0;
+}
+
+float calculate_distance(Point* p1, Point* p2)
+{
+    float dx, dy, dist;
+
+    dx = p2->x - p1->x;
+    dy = p2->x - p1->y;
+    dist = sqrt(pow(dx, 2) + pow(dy, 2));
+    
+    return dist;
+}
