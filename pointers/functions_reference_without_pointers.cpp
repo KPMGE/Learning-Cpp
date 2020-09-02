a0b715d04c98e6c4c0cf3d0547f9d43cb0dd6b65
KPMGE <jesuskevin254@gmail.com>
2020-09-02 16:08:36 -0300

    Adding example passing by reference without pointers

--- pointers/functions_reference_without_pointers.cpp
+++ pointers/functions_reference_without_pointers.cpp
@@ -1,27 +1,31 @@
-// C++ Program to insert and display data entered by using pointer notation.
-
 #include <iostream>
 using namespace std;
 
-int main(void) 
+// function definition to swap values
+void swap(int &n1, int &n2) 
 {
-    float arr[5];
-    
-   // Insert data using pointer notation
-    cout << "Enter 5 numbers: ";
-    for (int i = 0; i < 5; ++i)
-    {
-        // store input number in arr[i]
-        cin >> *(arr + i) ;
-    }
+    int temp;
+    temp = n1;
+    n1 = n2;
+    n2 = temp;
+}
 
-    // Display data using pointer notation
-    cout << "Displaying data: " << endl;
-    for (int i = 0; i < 5; ++i)
-    {
-        // display value of arr[i]
-        cout << *(arr + i) << endl ;
-    }
+int main(void)
+{
+
+    // initialize variables
+    int a = 1, b = 2;
+
+    cout << "Before swapping" << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    // call function to swap numbers
+    swap(a, b);
+
+    cout << "\nAfter swapping" << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
 
     return 0;
 }
