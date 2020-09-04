16bbf1e8843268f11331608c11d05135ada64b3e
KPMGE <jesuskevin254@gmail.com>
2020-09-04 07:41:56 -0300

    Update function isPrime

--- others/prime.cpp
+++ others/prime.cpp
@@ -28,7 +28,7 @@ bool isPrime(int n)
 
     else if(n > 0)
     {
-        for(int i = 2; i < n; i++)
+        for(int i = 2; i <= n/2; i++)
         {
             if(n % i == 0)
                 return false;
@@ -37,7 +37,7 @@ bool isPrime(int n)
     }
     else
     {
-        for(int i = -2; i > n; i--)
+        for(int i = -2; i >= n/2; i--)
         {
             if(n % i == 0)
                 return false;
