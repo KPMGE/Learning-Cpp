a1d8dfb7509c1bd27a96102f3cf848a9458b76ca
KPMGE <jesuskevin254@gmail.com>
2020-09-07 08:22:35 -0300

    Adding example of hierarchical inheritance in C++ programming language

---
+++ inheritance/hierarchical_inheritance.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class science
+{
+};
+
+class physics : public science
+{
+};
+
+class chemistry : public science
+{
+};
+
+class biology : public science
+{
+};
+
+int main(void)
+{
+
+    return 0;
+}
